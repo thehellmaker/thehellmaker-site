868288875ad62e90cce8b801246c38fb92236cd5
thehellmaker <thehellmaker@gmail.com>
2025-02-16 12:02:31 +0530

    revert redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,10 +8,10 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
   site: 'https://thehellmaker.com',
   integrations: [mdx(), sitemap()],
-  output: "server",
-  adapter: cloudflare(),
-  redirects: {
-    '/blog/how-deploy-deepseek-r1-671b-model-multi-node-multi-gpu-using-vllm-ray-cluster': 'https://github.com/thehellmaker/llm-experiments/blob/main/deepseek-r1/README.md',
-    '/blog/event-loops-multi-threading-and-multi-processing': 'https://medium.com/@thehellmaker/event-loops-multi-threading-and-multi-processing-b57e65cf364f'
-  }
+  // output: "server",
+  // adapter: cloudflare(),
+  // redirects: {
+  //   '/blog/how-deploy-deepseek-r1-671b-model-multi-node-multi-gpu-using-vllm-ray-cluster': '',
+  //   '/blog/event-loops-multi-threading-and-multi-processing': ''
+  // }
 });
